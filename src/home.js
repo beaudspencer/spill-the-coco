79bfcd53a5c62137bd7e4d53c3ec5fd245348372
Beau Spencer <bdspencer416@gmail.com>
2019-01-02 22:58:42 +0000

    added styles to the app bar and refactored google signin to call one of two callBack functions based on the success of a login. #1

--- src/home.js
+++ src/home.js
@@ -3,14 +3,53 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  Typography
+  Typography,
+  withStyles
 } from '@material-ui/core'
+import { generateKeyPair } from 'crypto';
+
+const styles = {
+  signin: {
+    position: 'absolute',
+    right: '1rem'
+  }
+}
+
+const Navi = withStyles({
+  positionSticky: true
+})(AppBar)
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.handleSuccess = this.handleSuccess.bind(this)
+    this.handleFailure = this.handleFailure.bind(this)
+  }
+  handleSuccess(googleUser) {
+  const profile = googleUser.getBasicProfile()
+  console.log('ID: ' + profile.getId())
+  console.log('Name: ' + profile.getName())
+  console.log('Image URL: ' + profile.getImageUrl())
+  console.log('Email: ' + profile.getEmail())
+  }
+  handleFailure() {
+    console.log('Fail')
+  }
+  componentDidMount() {
+    gapi.signin2.render('g-signin2', {
+      'scope': 'profile email',
+      'width': 120,
+      'height': 40,
+      'longtitle': false,
+      'theme': 'light',
+      'onsuccess': this.handleSuccess,
+      'onfailure': this.handleFailure
+    })
+  }
   render() {
     return (
-      <AppBar
-        color="Primary"
+      <Navi
+        color="primary"
       >
         <Toolbar>
           <Typography
@@ -19,8 +58,13 @@ export default class Home extends Component {
           >
             Spill The Coco
           </Typography>
+          <div
+            style={styles.signin}
+          >
+            <div id="g-signin2" data-onsuccess={this.onSignIn} />
+          </div>
         </Toolbar>
-      </AppBar>
+      </Navi>
     )
   }
 }
