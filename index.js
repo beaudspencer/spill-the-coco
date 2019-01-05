69e9fabe2f24208be60fe78fb122bed1ec8054c2
Beau Spencer <bdspencer416@gmail.com>
2019-01-05 22:45:30 +0000

    created get and put listeners on about that get/create the about page and return and that update the about content. #3

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 require('dotenv/config')
+
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 
 app.use(express.static('public'))
+
+app.use(bodyParser.json())
+
+app.get('/about', (req, res) => {
+  MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err
+    const db = client.db('local')
+    const collection = db.collection('about')
+    collection.find({post: 'about'}).toArray((err, items) => {
+      if (err) throw err
+      if (items.length === 0) {
+        collection.insertOne({
+          post: 'about',
+          text: 'empty'
+        }).then(() => {
+          collection.find({post: 'about'}).toArray((err, items) => {
+            if (err) throw err
+            res.json(items[0])
+          })
+        })
+        client.close()
+        return
+      }
+      res.json(items[0])
+      client.close()
+    })
+  })
+})
+
+app.put('/about', (req, res) => {
+  MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err
+    const db = client.db('local')
+    const collection = db.collection('about')
+    collection.updateOne({post: 'about'}, {$set: {text: req.body.text}}, (err) => {
+      if (err) throw err
+      collection.find({post: 'about'}).toArray((err, items) => {
+        if (err) throw err
+        res.json(items[0])
+        client.close()
+      })
+    })
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log('Server is listening on port', process.env.PORT)
