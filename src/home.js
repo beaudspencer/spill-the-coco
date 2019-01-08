e4442a2607e78e73e41187702ea41104329c8ab1
Beau Spencer <bdspencer416@gmail.com>
2019-01-08 21:12:33 +0000

    refactored component to use grid layout and added category buttons. #5

--- src/home.js
+++ src/home.js
@@ -1,22 +1,40 @@
 import React from 'react'
 import {
   Typography,
-  withStyles
+  withStyles,
+  Grid
 } from '@material-ui/core'
 import HomeAbout from './home-about'
+import HomeCategory from './home-category'
 
 const styles = {
-  homeAboutD: {
-    width: '33%',
+  homeAbout: {
+    width: '40%',
+    padding: '1rem',
+    maxWidth: '20rem',
+    minWidth: '300px',
     float: 'right',
-    marginTop: '3rem'
-  },
-  homeAboutM: {
     marginTop: '3rem'
   },
   container: {
     margin: '0 auto',
-    width: '80%'
+    width: '80%',
+    maxWidth: '80rem'
+  },
+  categories: {
+    padding: '1rem',
+    marginTop: '3rem',
+    width: '60%',
+    maxWidth: '60rem',
+    minWidth: '300px'
+  },
+  divider: {
+    borderLeftStyle: 'solid',
+    borderLeftColor: '#e6e5a0',
+    width: '4px',
+    margin: '3rem 3rem 0 3rem',
+    height: '20rem',
+    padding: '0'
   }
 }
 
@@ -27,6 +45,18 @@ const DontSpill = withStyles({
     textAlign: 'center'
   }
 })(Typography)
+
+const categories = [
+  {
+    title: 'Food'
+  },
+  {
+    title: 'Life'
+  },
+  {
+    title: 'Travel'
+  }
+]
 
 export default function Home(props) {
   const { mobile } = props
@@ -39,15 +69,41 @@ export default function Home(props) {
       >
         Spill the Coco
       </DontSpill>
-      <div
-        style={
-          !mobile
-            ? styles.homeAboutD
-            : styles.homeAboutM
-        }
+      <Grid
+        container
+        spacing={32}
+        justify="center"
+        alignItems="center"
       >
-        <HomeAbout/>
-      </div>
+        <Grid
+          item
+          style={styles.categories}
+        >
+          {
+            categories.map((category, index) => {
+              return (
+                <HomeCategory
+                  key={index}
+                  category={category}
+                />
+              )
+            })
+          }
+        </Grid>
+        {
+          !mobile && <Grid
+            item
+            style={styles.divider}
+          >
+          </Grid>
+        }
+        <Grid
+          item
+          style={styles.homeAbout}
+        >
+          <HomeAbout/>
+        </Grid>
+      </Grid>
     </div>
   )
 }
