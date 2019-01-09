b7eaa3d6c846798466e9535feb793d6843c096ff
Beau Spencer <bdspencer416@gmail.com>
2019-01-09 23:50:37 +0000

    added get and put listeners for the category listener that takes a category query param to update or grab the correct page. #5

--- index.js
+++ index.js
@@ -49,11 +49,27 @@ app.put('/about', (req, res) => {
         }
       }, (err) => {
         if (err) throw err
-        collection.find({post: 'about'}).toArray((err, items) => {
-          if (err) throw err
-          res.json(items[0])
-          client.close()
-        })
+        res.sendStatus(200)
+      })
+    })
+  }
+  else res.sendStatus(403)
+})
+
+app.put('/category', (req, res) => {
+  const category = req.query.cat
+  if (req.query.id === process.env.ADMIN_ID) {
+    MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+      if (err) throw err
+      const db = client.db('local')
+      const collection = db.collection(category)
+      collection.updateOne({post: category}, {
+        $set: {
+          text: req.body.text
+        }
+      }, (err) => {
+        if (err) throw err
+        res.sendStatus(200)
       })
     })
   }
