9344158bcee8f47348cde5b09435b7e0e97b9b97
Beau Spencer <bdspencer416@gmail.com>
2019-01-11 23:28:08 +0000

    added a menu with addition options for the post. #6

--- src/post-creator.js
+++ src/post-creator.js
@@ -2,22 +2,54 @@ import React from 'react'
 import {
   Card,
   CardContent,
-  CardActionArea,
+  TextField,
+  Typography,
+  Menu,
+  MenuItem,
   CardActions,
-  TextField
+  withStyles,
+  Fab
 } from '@material-ui/core'
+import {
+  Add
+} from '@material-ui/icons'
+
+const styles = {
+  actions: {
+    margin: '0.5rem auto'
+  }
+}
+
+const GreenFab = withStyles({
+  root: {
+    backgroundColor: '#c4e6a0'
+  }
+})(Fab)
 
 export default class PostCreator extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      anchorEl: null,
       header: '',
       description: '',
       content: []
     }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
+  handleClick(event) {
+    this.setState({
+      anchorEl: event.currentTarget
+    })
+  }
+  handleClose() {
+    this.setState({
+      anchorEl: null
+    })
   }
   render() {
-    const { header, description } = this.state
+    const { header, description, anchorEl } = this.state
     return (
       <div>
         <Card>
@@ -33,10 +65,47 @@ export default class PostCreator extends React.Component {
               value={description}
             />
           </CardContent>
-          <CardActionArea>
-            <CardActions>
-            </CardActions>
-          </CardActionArea>
+          <CardActions>
+            <div
+              style={styles.actions}
+            >
+              <GreenFab
+                aria-owns={anchorEl ? 'add-menu' : undefined}
+                aria-haspopup="true"
+                onClick={this.handleClick}
+              >
+                <Add/>
+              </GreenFab>
+              <Menu
+                id="add-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleClose}
+              >
+                <MenuItem>
+                  <Typography
+                    variant="h6"
+                  >
+                    Header
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Typography
+                    variant="h6"
+                  >
+                    Text
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <Typography
+                    variant="h6"
+                  >
+                    Image
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </div>
+          </CardActions>
         </Card>
       </div>
     )
